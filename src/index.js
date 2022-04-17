aa41af656b0638825b7fb7feec2f25104962f60f
kryshtafovich.nastya <miksturchik123@gmail.com>
2022-04-17 20:03:47 +0300

    init (task-12) : add solution for task 12

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import Text from "./Text";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <Text
+            text="Different text"
+            textColor="red"
+            textSize="50px"
+            underline={false}
+        />
+        <Text
+            text="Different color"
+            textColor="yellow"
+            textSize="40px"
+            underline={false}
+        />
+        <Text
+            text="Different size"
+            textColor="green"
+            textSize="35px"
+            underline={false}
+        />
+        <Text
+            text="Underlined"
+            textColor="orange"
+            textSize="30px"
+            underline={true}
+        />
     </React.StrictMode>,
 );
